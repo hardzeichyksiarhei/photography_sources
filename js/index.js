c098436cc96b34950f8cfe11ca6fa2b4111a7903
hardz <hardzeichyksiarhei@gmail.com>
2017-11-13 22:09:55 +0300

    fix v.1.0

--- js/index.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([2],{10:function(i,n){},9:function(i,n,o){"use strict";(function(i){o(1),o(10),o(2),i(function(){i("#portfolio-grid").justifiedGallery({rowHeight:320,margins:20})})}).call(n,o(0))}},[9]);
--- js/portfolio.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([1],{11:function(i,t,n){"use strict";(function(i){n(1),n(12),n(2),i(function(){i("#portfolio-grid").justifiedGallery({rowHeight:200,margins:20,lastRow:"justify"})})}).call(t,n(0))},12:function(i,t){}},[11]);
